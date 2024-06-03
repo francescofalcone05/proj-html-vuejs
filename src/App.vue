68f6ccb55b281d1d48e197081a5e1c617d46c9d9
francescofalcone05 <ciccio_97@live.it>
2024-06-03 16:54:35 +0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,42 @@
+<script>
+import AppHeader from './components/AppHeader.vue';
+import AppMain from './components/AppMain.vue';
+import AppFooter from './components/AppFooter.vue';
+import store from './data/store.js';
+
+
+
+export default {
+  components: {
+    AppHeader,
+    AppMain,
+    AppFooter
+  },
+
+  data() {
+    return {
+      store,
+
+    }
+  },
+  methods: {
+
+  },
+  created() {
+
+  },
+  mounted() {
+
+  },
+  computed: {
+
+  },
+}
+
+</script>
+
+<template>
+
+</template>
+
+<style scoped></style>
---
+++ src/components/AppFooter.vue
@@ -0,0 +1,38 @@
+<script>
+import store from '../data/store.js';
+
+
+
+export default {
+    components: {
+
+
+    },
+
+    data() {
+        return {
+            store,
+
+        }
+    },
+    methods: {
+
+    },
+    created() {
+
+    },
+    mounted() {
+
+    },
+    computed: {
+
+    },
+}
+
+</script>
+
+<template>
+
+</template>
+
+<style scoped></style>
